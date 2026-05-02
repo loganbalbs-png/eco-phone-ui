dc440aee0dfd09223492f8f617919b1ec858ba19
loganbalbs-png <loganbalbs@gmail.com>
2026-05-02 19:11:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 function openApp(app) {
-  alert("Opening " + app);
+  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
+  document.getElementById(app).classList.add("active");
+
+  if (app === "camera") startCamera();
+}
+
+function goHome() {
+  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
+  document.getElementById("home").classList.add("active");
+}
+
+/* TRAVEL WINDOW */
+function openWindow() {
+  const windowDiv = document.getElementById("planeWindow");
+  windowDiv.style.background = "url('assets/rio.jpg') center/cover";
+  windowDiv.innerHTML = "";
+}
+
+/* CAMERA */
+function startCamera() {
+  const video = document.getElementById("cam");
+
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+      video.srcObject = stream;
+    });
+}
+
+function takePhoto() {
+  const video = document.getElementById("cam");
+  const canvas = document.getElementById("photo");
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0);
 }
